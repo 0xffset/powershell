f0f4b5697b1d5b774e71f7c591971a8968c55cbd
roleyder <roleyder02@gmail.com>
2020-12-13 12:06:14 -0800

    regFind

---
+++ regFind.sh
@@ -0,0 +1,11 @@
+dir \/s/b | find /I"password"
+dir \/b /s "*.bat"
+dir \/b /s "*.cmd"
+dir \/b /s "*.vbs"
+dir \/b /s "*.vba"
+dir \/b /s "*.vbe"
+dir \/b /s "*.ps1"
+dir \/b /s "*.config"
+dir \/b /s "*.ini"
+dir /s "*pass* == *cred* == *vnc* == *.config*
+findstr /si password *.xml *.ini *.txt
