daf19e9cf1e69140222cd7adc520a87df515be34
rolEYder <49726414+RolEYder@users.noreply.github.com>
2021-04-10 10:42:22 -0700

    Basic Windows Information Machine

---
+++ BasicInfo.ps1
@@ -0,0 +1,11 @@
+﻿Write-Output "Computer name is":
+Get-Content env:computername
+Write-Output "Windows version is:"
+(Get-WmiObject -Class Win32_OperatingSystem)
+Get-WmiObject Win32_Processor | findstr "Name"
+Write-Output "Total Memory is":
+[Math]::Round((Get-WmiObject -Class win32_computersystem -ComputerName localhost).TotalPhysicalMemory1Gb)
+Write-Output "Diks":
+Get-WmiObject -Class Win32_logicaldisk -Filter "DriveType = '3'"
+Write-Output "IPV4 addresses"
+Get-NetIPAddress -AddressFamily IPv4 | Sort-Object -Property InterfaceIndex | Format-Table
